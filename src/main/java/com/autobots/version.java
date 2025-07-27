fe66ea2909b1e0f2f142d955d3dd7f0a5f4220b2
Badam <badamturganbaeva@mail.ru>
2025-07-27 05:04:51 -0700

    Fix changes

--- src/main/java/com/autobots/version.java
+++ src/main/java/com/autobots/version.java
@@ -2,13 +2,8 @@ package com.autobots;
 
 public class version {
     public static void main(String[] args) {
-badam/version-1
-        hdhhdjdksdml
-version-4.12.5
-        fewgfhlksao;ldekfhwjhriuqweislkaj
 
         System.out.println("New version 4.0.1");
-main
-main
+
     }
 }
